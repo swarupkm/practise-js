90ac28cf16ac26f1575922c7f1209e64e50ca855
swarupkm <swarupmahapatra1@gmail.com>
2018-11-17 15:11:36 +0530

    Added test for modelling the parkinglot

---
+++ parkinglot/parkinglot.js
@@ -0,0 +1,37 @@
+const createParkingLot = ({ size }) => {
+  const NO_CAR = null;
+  let availableSlots = size;
+  const lot = {};
+  for (let i = 1; i <= size; i += 1) { lot[i] = NO_CAR; }
+
+  return {
+    size: () => Object.keys(lot).length,
+
+    park: (car) => {
+      if (availableSlots === 0) throw Error('Parking Lot is Full');
+
+      for (let i = 1; i <= size; i += 1) {
+        if (lot[i] === NO_CAR) {
+          lot[i] = car;
+          availableSlots -= 1;
+          return;
+        }
+      }
+    },
+
+    availableSlots: () => availableSlots,
+
+    getCarAtSlot: slotNumber => lot[slotNumber],
+
+  };
+};
+
+const createCar = config => ({
+  regNumber: config.regNumber,
+  color: config.color,
+});
+
+module.exports = {
+  createParkingLot,
+  createCar,
+};
---
+++ parkinglot/parkinglot.test.js
@@ -0,0 +1,24 @@
+import { createParkingLot, createCar } from './parkinglot';
+
+describe('parkinglot', () => {
+  it('should create a parking lot of certain size', () => {
+    const lot = createParkingLot({ size: 6 });
+    expect(lot.size()).toBe(6);
+  });
+
+  it('should park car in a parking lot ', () => {
+    const lot = createParkingLot({ size: 6 });
+    const car = createCar({ regNumber: '123123', color: 'white' });
+    lot.park(car);
+    expect(lot.availableSlots()).toBe(5);
+    expect(lot.getCarAtSlot(1)).toEqual(car);
+  });
+
+  it('should thow error if parking lot is full and car is parked', () => {
+    const lot = createParkingLot({ size: 1 });
+    const car1 = createCar({ regNumber: '123123', color: 'white' });
+    const car2 = createCar({ regNumber: '321321', color: 'black' });
+    lot.park(car1);
+    expect(() => { lot.park(car2); }).toThrow();
+  });
+});
